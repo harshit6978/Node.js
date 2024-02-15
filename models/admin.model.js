9f980a0bbfaf7f37ed2068a2fce5785d91983279
harshit <126167058+harshit6978@users.noreply.github.com>
2024-02-15 14:58:41 +0530

    node js

---
+++ models/admin.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+
+const adminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    mobileno: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        require: true
+    },
+    role: {
+        type: String,
+        enum: [user, admin],
+        required: true,
+    }
+})
+
+const admin = mongoose.model("adminSchema", adminSchema);
